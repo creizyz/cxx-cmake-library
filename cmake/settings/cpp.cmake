1e7b08c68b951e4e9cdf40f88873e9c461906dbf
Rémi Laot <remilaot@gmail.com>
2020-10-19 11:29:14 +0200

    WIP - improvements

--- cmake/settings/cpp.cmake
+++ cmake/settings/cpp.cmake
@@ -1,34 +1,20 @@
 # ================================================================== #
+# ================================================== # Global compile Options
+
+if(MSVC)
+  add_compile_options(/W4 /WX)
+else()
+  add_compile_options(-W -Wall -Wextra -Werror -pedantic)
+endif()
+
+# ================================================================== #
 # ================================================== # C++ Standard
 
-set(${PROJECT_NAME}_CXX_STANDARD cxx_std_20)
+target_compile_features(${TARGET_NAME} PUBLIC cxx_std_20)
 
 # ================================================================== #
-# ================================================== # CLANG flags
-if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-  set(${PROJECT_NAME}_CXX_FLAGS
-    # nothing
-  )
-# ================================================================== #
-# ================================================== # GCC flags
-elseif (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-  set(${PROJECT_NAME}_CXX_FLAGS
-    -Wall
-    -Wextra
-    -pedantic
-    -Werror
-  )
-# ================================================================== #
-# ================================================== # Intel flags
-elseif (CMAKE_CXX_COMPILER_ID MATCHES "Intel")
-  set(${PROJECT_NAME}_CXX_FLAGS
-    # nothing
-  )
-# ================================================================== #
-# ================================================== # GCC flags
-elseif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-  set(${PROJECT_NAME}_CXX_FLAGS
-    /W4
-    /WX
-  )
-endif()
+# ================================================== # More options
+
+# ...
+# ...
+# ...
